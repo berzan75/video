a6eb2f9f3e662df0f083e718f8f54a917c1ea79f
berzan75 <issundee358@gmail.com>
2026-03-09 19:23:46 +0100

    Update video.py

--- video.py
+++ video.py
@@ -2,15 +2,15 @@ import os
 import cv2
 from PIL import Image
 
-os.chdir(r"C:\Users\berza\OneDrive\Documenten\cv\video using images\images")
-path = os.getcwd()
+os.chdir("C:\\Users\\berza\\OneDrive\\Documenten\\cv\\video using images\\images")
+path ="C:\\Users\\berza\\OneDrive\\Documenten\\cv\\video using images\\images"
 
 mean_height=0
 mean_width=0
-image_files = [f for f in os.listdir(path) if f.endswith(('.jpg', '.jpeg', '.png'))]
-number_of_images=len(image_files)
+#image_files = [f for f in os.listdir(path) if f.endswith(('.jpg', '.jpeg', '.png'))]
+number_of_images=len(os.listdir('.'))
 
-for i in image_files:
+for i in os.listdir('.'):
     img=Image.open(os.path.join(path,i))
     width,height=img.size
     mean_width=mean_width+width
@@ -25,7 +25,7 @@ for i in os.listdir('.'):
         img=Image.open(os.path.join(path,i))
         width,height=img.size
         print(height,width)
-        image_resize=img.resize((mean_width,mean_height),Image.ANTIALIAS)
+        image_resize=img.resize((mean_width,mean_height),Image.Resampling.LANCZOS)
         image_resize.save(i,"JPEG",quality=95)
 def video_generator():
     video_name="video.avi"
@@ -33,7 +33,7 @@ def video_generator():
     images=[]
     for i in os.listdir('.'):
         if i.endswith('.jpg') or i.endswith('.jpeg') or i.endswith('.png'):
-            images.append(img)
+            images.append(i)
     frame=cv2.imread(os.path.join(".",images[0]))
     height,width,layers=frame.shape
 
